cb087987c6f2e80cc9bd1c08b3eab70c58b44948
Glenn <glenn906@gmail.com>
2018-09-02 09:42:33 -0600

    Testing Jenkins

--- app/src/main/java/com/pencelab/testproject/MainActivity.kt
+++ app/src/main/java/com/pencelab/testproject/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.pencelab.testproject
 
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -18,5 +22,29 @@ class MainActivity : AppCompatActivity() {
         val versionName = BuildConfig.VERSION_NAME
 
         return "Version: $versionName (code $versionCode)"
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item!!.itemId){
+            R.id.menu_google_play -> this.openMarket()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun openMarket(){
+        try {
+            val playstoreuri: Uri = Uri.parse("market://details?id=" + packageName)
+            val intent = Intent(Intent.ACTION_VIEW, playstoreuri)
+            startActivity(intent)
+        }catch (exp:Exception){
+            val playstoreuri: Uri = Uri.parse("http://play.google.com/store/apps/details?id=" + packageName)
+            val intent = Intent(Intent.ACTION_VIEW, playstoreuri)
+            startActivity(intent)
+        }
     }
 }
