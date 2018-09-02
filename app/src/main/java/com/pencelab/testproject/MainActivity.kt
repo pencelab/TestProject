a0c914d12a5ccaafb5ba90f4fe4d38a775ee179c
Glenn <glenn906@gmail.com>
2018-09-02 05:53:16 -0600

    Testing Jenkins

--- app/src/main/java/com/pencelab/testproject/MainActivity.kt
+++ app/src/main/java/com/pencelab/testproject/MainActivity.kt
@@ -2,11 +2,21 @@ package com.pencelab.testproject
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textViewVersion.setText(this.getApplicationVersion())
+    }
+
+    fun getApplicationVersion() : String {
+        val versionCode = BuildConfig.VERSION_CODE
+        val versionName = BuildConfig.VERSION_NAME
+
+        return "Version: $versionName (code $versionCode)"
     }
 }
