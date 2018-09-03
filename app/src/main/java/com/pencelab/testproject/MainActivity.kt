f3549b013f73fc0edea2556599aaf8c1767703d8
Glenn <glenn906@gmail.com>
2018-09-03 12:50:10 -0600

    Launcher icon fixed.

--- app/src/main/java/com/pencelab/testproject/MainActivity.kt
+++ app/src/main/java/com/pencelab/testproject/MainActivity.kt
@@ -10,30 +10,11 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    //private var root: Node? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         textViewVersion.text = this.getApplicationVersion()
-
-
-        /*val node10: Node = Node(null, null)
-
-        val node7: Node = Node(null, null)
-        val node8: Node = Node(null, null)
-        val node9: Node = Node(node10, null)
-
-        val node4: Node = Node(null, node7)
-        val node5: Node = Node(null, null)
-        val node6: Node = Node(node8, node9)
-
-        val node2: Node = Node(node4, node5)
-        val node3: Node = Node(null, node6)
-
-        val node1: Node = Node(node2, node3)*/
-
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -60,7 +41,7 @@ class MainActivity : AppCompatActivity() {
             val playStoreURI: Uri = Uri.parse("market://details?id=$packageName")
             val intent = Intent(Intent.ACTION_VIEW, playStoreURI)
             startActivity(intent)
-        }catch (exp:Exception){
+        }catch (e: Exception){
             val playStoreURI: Uri = Uri.parse("http://play.google.com/store/apps/details?id=$packageName")
             val intent = Intent(Intent.ACTION_VIEW, playStoreURI)
             startActivity(intent)
--- app/src/test/java/com/pencelab/testproject/MainActivityTest.kt
+++ app/src/test/java/com/pencelab/testproject/MainActivityTest.kt
@@ -1,5 +1,6 @@
 package com.pencelab.testproject
 
+import android.app.Activity
 import org.junit.Before
 import org.junit.Test
 
@@ -7,11 +8,12 @@ import org.junit.runner.RunWith
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.Shadows
-import org.robolectric.shadows.ShadowActivity
 import android.content.Intent
+import android.net.Uri
 import org.robolectric.fakes.RoboMenuItem
 import org.junit.Assert.*
-
+import org.robolectric.RuntimeEnvironment
+import org.mockito.Mockito
 
 @RunWith(RobolectricTestRunner::class)
 class MainActivityTest {
@@ -24,11 +26,23 @@ class MainActivityTest {
     }
 
     @Test
-    fun clickingMenuGooglePlay_shouldLaunchIntent() {
-        val item = RoboMenuItem(R.id.menu_google_play)
-        this.mainActivity.onOptionsItemSelected(item)
-        val shadowActivity: ShadowActivity = Shadows.shadowOf(this.mainActivity)
-        val startedIntent: Intent = shadowActivity.nextStartedActivity
-        assertEquals(startedIntent.action, Intent.ACTION_VIEW)
+    fun clickingMenuGooglePlay_shouldLaunchIntentFromTryBlock(){
+        val expectedIntent = Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=${RuntimeEnvironment.application.packageName}"))
+        this.mainActivity.onOptionsItemSelected(RoboMenuItem(R.id.menu_google_play))
+        val shadowActivity = Shadows.shadowOf(this.mainActivity)
+        val startedIntent = shadowActivity.nextStartedActivity
+        assertTrue(expectedIntent.filterEquals(startedIntent))
+    }
+
+    @Test
+    fun clickingMenuGooglePlay_shouldLaunchIntentFromCatchBlock(){
+        val spyActivity: Activity = Mockito.spy(this.mainActivity)
+        Mockito.doThrow(RuntimeException()).doCallRealMethod().`when`(spyActivity).startActivity(Mockito.any())
+
+        val expectedIntent = Intent(Intent.ACTION_VIEW, Uri.parse("http://play.google.com/store/apps/details?id=${RuntimeEnvironment.application.packageName}"))
+        spyActivity.onOptionsItemSelected(RoboMenuItem(R.id.menu_google_play))
+        val shadowActivity = Shadows.shadowOf(spyActivity)
+        val startedIntent = shadowActivity.nextStartedActivity
+        assertTrue(expectedIntent.filterEquals(startedIntent))
     }
 }
